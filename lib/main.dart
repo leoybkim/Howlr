fb0dd85a3c9713535cdbda285963f60e46a3050f
leoybkim <leoybkim@gmail.com>
2017-08-30 23:50:55 -0400

    Support images

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,10 @@ import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_database/ui/firebase_animated_list.dart';
-
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:math';
+import 'dart:io';
 
 void main() {
   runApp(new MyApp());
@@ -61,8 +64,7 @@ class ChatScreenState extends State<ChatScreen> {
         elevation:
           Theme.of(context).platform == TargetPlatform.iOS? 0.0 : 4.0,
       ),
-      body: new Container(
-        child : new Column(
+      body: new Column(
           children: <Widget>[
             new Flexible(
               child: new FirebaseAnimatedList(
@@ -86,11 +88,6 @@ class ChatScreenState extends State<ChatScreen> {
             )
           ],
         ),
-        decoration: Theme.of(context).platform == TargetPlatform.iOS
-          ? new BoxDecoration(
-          border:
-          new Border(top: new BorderSide(color: Colors.grey[200])))
-          : null),
     );
   }
 
@@ -101,6 +98,21 @@ class ChatScreenState extends State<ChatScreen> {
         margin: const EdgeInsets.symmetric(horizontal: 8.0),
         child: new Row(
           children: <Widget>[
+            new Container(
+              margin: new EdgeInsets.symmetric(horizontal: 4.0),
+              child: new IconButton(
+                icon: new Icon(Icons.photo_camera),
+                onPressed: () async {
+                  await _ensureLoggedIn();
+                  File imageFile = await ImagePicker.pickImage();
+                  int random = new Random().nextInt(100000);
+                  StorageReference ref = FirebaseStorage.instance.ref().child("image_$random.jpg");
+                  StorageUploadTask uploadTask = ref.put(imageFile);
+                  Uri downloadUrl = (await uploadTask.future).downloadUrl;
+                  _sendMessage(imageUrl: downloadUrl.toString());
+                }
+              ),
+            ),
             new Flexible(
               child: new TextField(
                 controller: _textController,
@@ -144,10 +156,11 @@ class ChatScreenState extends State<ChatScreen> {
     _sendMessage(text: text);
   }
 
-  void _sendMessage({ String text }) {
+  void _sendMessage({ String text, String imageUrl }) {
     // push creates new row, set populates with data
     reference.push().set({
       'text': text,
+      'imageUrl': imageUrl,
       'senderName': googleSignIn.currentUser.displayName,
       'senderPhotoUrl': googleSignIn.currentUser.photoUrl,
     });
@@ -204,7 +217,9 @@ class ChatMessage extends StatelessWidget {
                     style: Theme.of(context).textTheme.subhead),
                 new Container(
                   margin: const EdgeInsets.only(top: 5.0),
-                  child: new Text(snapshot.value['text']),
+                  child: snapshot.value['imageUrl'] != null ?
+                    new Image.network(snapshot.value['imageUrl'], width: 250.0,) :
+                    new Text(snapshot.value['text']),
                 ),
               ],
             ),
